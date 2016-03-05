528525acb44fc97d051c5d200825c5ab1895e312
Yehoshua Santiago Meléndez <Yehoshua Santiago Melndez>
2016-03-05 14:00:28 -0600

    Se sube el proyecto

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name localclientApp
+ * @description
+ * # localclientApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('localclientApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
